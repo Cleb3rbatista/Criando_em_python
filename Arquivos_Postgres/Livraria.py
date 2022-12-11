cdf15f7dd99ed6cabff34ed587724c3695f2b695
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2022-12-11 19:31:02 -0300

    corrigindo exceções de usuário

--- Arquivos_Postgres/Livraria.py
+++ Arquivos_Postgres/Livraria.py
@@ -7,22 +7,41 @@ def comprar_livros():
                         password='postgres')
     cursor = conexao.cursor()
 
-    cursor.execute("SELECT id_livro , livro ,preco FROM public.livros")
+    cursor.execute("SELECT id_livros , livro ,preco FROM public.livros")
 
     for linha in cursor.fetchall():
-        print(f"numero de indetificação do livro {linha[0]}, nome do livro {linha[1]}, preço {linha[2]},")
-        
-    deseja_comprar = input("Deseja comprar um livro?\n [S]im ou [N]ão\n").upper()
-    if deseja_comprar == "N":
-        exit()
-        
-    elif deseja_comprar == "S":
-        idlivro_escolhido = int(input("Qual o numero do livro que voce deseja comprar?\n"))
-        cursor.execute(f"SELECT id_livro  FROM public.livros WHERE id_livro = '{idlivro_escolhido}'")
-        if cursor.fetchone==[]:
-            print("Livro não encontrado")
+        print(f" {linha[0]}) {linha[1]} preço: {linha[2]},")
+    while True:
+        deseja_comprar = input("Deseja comprar um livro?\n [S]im ou [N]ão\n").upper()
+        if deseja_comprar == "N":
+            print("Saindo...")
+            exit()
             
-        cursor.execute(f"SELECT *  FROM public.livros WHERE id_livro = '{idlivro_escolhido}'")
+        elif deseja_comprar == "S":
+            while True:
+                try:
+                    
+                    idlivro_escolhido = int(input("Qual o numero do livro que voce deseja comprar?\n"))
+                    cursor.execute(f"SELECT id_livros  FROM public.livros")
+                    qtd_livros =len(cursor.fetchall())
+                    if idlivro_escolhido > qtd_livros:
+                        print(f"Atualmente a somete {qtd_livros} na biblioteca")   
+                except ValueError:
+                    
+                    print()
+                    print("Digite um numero inteiro\n tente novamente")
+                    print()
+                    continue
+                else:
+                    break
+            
+        else:
+            print()
+            print("opção invalida!\n tente novamente")
+            print()
+            continue
+        
+        cursor.execute(f"SELECT *  FROM public.livros WHERE id_livros = '{idlivro_escolhido}'")
         
         for livro_para_comprar in cursor.fetchall():
             confirmando_livro = input(f"O livro escolhido e {livro_para_comprar[1]}?\n [S]im ou [N]ão\n").upper()
@@ -31,7 +50,7 @@ def comprar_livros():
                 print(f"O livro {livro_para_comprar[1]} custa {livro_para_comprar[6]}")
                 
             elif confirmando_livro == "N":
-                escolher_novamente=input("lamento, deseja escolher novamente?\n [S]im ou [N]ão\n")
+                escolher_novamente=input("lamento, deseja escolher novamente?\n [S]im ou [N]ão\n").upper()
                 
                 if escolher_novamente == "S":
                     os.system("cls")
@@ -45,11 +64,8 @@ def comprar_livros():
                     print("opção invalida!")
             else:
                 print("opção invalida!")           
-    else:
-        print("opção invalida!")
 
 
-
-    conexao.close()
-    cursor.close()
+        conexao.close()
+        cursor.close()
 comprar_livros()
